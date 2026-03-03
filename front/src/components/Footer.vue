90486c2b282dfc5d48577654902f0be798ec857f
Almaz Kambarov <almaz.kambarov@esynergy.lv>
2026-03-03 16:20:36 +0500

    some bug fixed

--- front/src/components/Footer.vue
+++ front/src/components/Footer.vue
@@ -9,7 +9,7 @@
                         :to="{ name: 'index' }"
                         class="header__logotype"
                     >
-                            <img src="/assets/icons/logo.webp" alt="" />
+                            <img src="/dist/assets/icons/logonew.svg" alt="" />
                         <!-- <span>{{ settings.site_name }}</span> -->
                     </router-link>
                             <nav class="header__nav gpu-boost">
