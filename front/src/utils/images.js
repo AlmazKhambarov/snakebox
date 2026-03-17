6575205c1c7cf684543c1ed899ea8c391972b682
Almaz Kambarov <almaz.kambarov@esynergy.lv>
2026-03-17 16:20:27 +0500

    `getImageFromStorage` now directly returns absolute URLs and uses the backend URL for relative paths.

--- front/src/utils/images.js
+++ front/src/utils/images.js
@@ -1,5 +1,8 @@
 const getImageFromStorage = (image) => {
-    return import.meta.env.VITE_APP_FRONTEND_URL + image;
+    if (image && typeof image === 'string' && image.startsWith('http')) {
+        return image;
+    }
+    return (import.meta.env.VITE_APP_BACKEND_URL || '') + (image || '');
 };
 
 export { getImageFromStorage };
